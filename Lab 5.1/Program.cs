0e5a36f6d30e79a15fd9bbf939ff5c3b2f8daad9
krist <krist@LAPTOP-HIQLHLL6>
2022-11-06 02:13:42 +0500

    Добавьте файлы проекта.

---
+++ Lab 5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sr,sum = 0;           
+            int[] array = new int[7];
+            for (int i = 0; i < 7; i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());
+                sum += array[i];
+            }
+            sr = sum / 7;
+            Console.WriteLine(" Среднее арифметическое = {0} ",sr);
+            Console.ReadKey();
+        }
+    }
+}
